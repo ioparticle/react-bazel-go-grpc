0f56ef39fba9e025c3845becb8e58ae165e1e188
Saman Shafigh <saman@canva.com>
2020-03-23 17:06:02 +1100

    Add react dom

---
+++ web/src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Home } from 'react_bazel_go_grpc/page/home/home';
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
